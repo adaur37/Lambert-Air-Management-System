5b42289d83290968e7c342473abcae4c216d36ee
Andrew J. Daur <92742783+adaur37@users.noreply.github.com>
2022-05-03 12:46:13 -0400

    Update index.php
    some css is needed for the cards, columns and rows. Will be adding that shortly.

--- LAMS_SE/about_us/index.php
+++ LAMS_SE/about_us/index.php
@@ -1,5 +1,54 @@
 <?php require_once("../view/header.php"); ?>
+<div class="about-us">
+  <h1>About the LAMS Team</h1>
+  <p>Some info about who we are as a team, and what roles everyone is responsible for.</p>
+</div>
 
+<h2 style="text-align:center">LAMS Team</h2>
+<div class="row">
+<!--  Andrew  -->
+   <div class="column">
+     <div class="card">
+       <img src="" alt="Andrew" style="width:100%">
+       <div class="container">
+         <h2>Andrew Daur</h2>
+         <p class="title">------</p>
+         <p>lorem10</p>
+         <p>ad1297@usnh.com</p>
+         <p><button class="button">Contact</button></p>
+       </div>
+     </div>
+  </div>
+
+<!-- Matthew -->
+  <div class="column">
+    <div class="card">
+       <img src="" alt="Matthew" style="width:100%">
+       <div class="container">
+         <h2>Matthew Lavin</h2>
+         <p class="title">------</p>
+         <p>lorem10</p>
+         <p>ad1297@usnh.com</p>
+         <p><button class="button">Contact</button></p>
+        </div>
+     </div>
+  </div>
+
+<!--  Josh  -->
+  <div class="column">
+     <div class="card">
+       <img src="" alt="Josh" style="width:100%">
+       <div class="container">
+         <h2>Joshua Snider</h2>
+         <p class="title">------</p>
+         <p>lorem10</p>
+         <p>ad1297@usnh.com</p>
+         <p><button class="button">Contact</button></p>
+       </div>
+      </div>
+    </div>
+<!-- End div class row -->
+</div>
 
 
 <?php require_once("../view/footer.php"); ?>
